ec6307cd90d0eb1a373b826dfad8e3cfd17b3d8a
kusmaSingh <kusma.singh04@gmail.com>
2024-01-12 16:37:38 -0500

    Home and Todo List

--- projects/host-app/src/app/app.module.ts
+++ projects/host-app/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routeCompArr } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routeCompArr
   ],
   imports: [
     BrowserModule,
@@ -16,3 +19,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
